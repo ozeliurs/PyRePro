a9b39c453543a2fd6b03ff053934a3c24ae0c6b1
Maxime BILLY <ozeliurs@gmail.com>
2022-12-15 15:05:03 +0100

    feat: add generate endpoint

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 from flask import Flask, jsonify, render_template
 
+from pyrepro.site_conf import NginxConf
+
 config_folder = Path(__file__).parent / "config"
 config_folder.mkdir(parents=True, exist_ok=True)
 
@@ -23,6 +25,13 @@ def get_list_config():
 @app.get('/config/<name>')
 def get_config(name):
     return jsonify(json.loads(config_folder.joinpath(f"{name}.json").read_text()))
+
+
+@app.get('/config/<name>/conf')
+def get_config_conf(name):
+    conf = NginxConf(config_folder.joinpath(f"{name}.conf"))
+    conf.load(config_folder.joinpath(f"{name}.json"))
+    return f"<pre>{conf.generate()}</pre>"
 
 
 if __name__ == '__main__':
